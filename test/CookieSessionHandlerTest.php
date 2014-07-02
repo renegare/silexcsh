484c7fcb62ab90b2e11ccc150328773eeb0cfe5f
Mudi Ugbowanko <mudi.ugbowanko@mendeley.com>
2014-07-02 15:31:06 +0100

    add main session handling class

--- test/CookieSessionHandlerTest.php
+++ test/CookieSessionHandlerTest.php
@@ -1,8 +1,11 @@
 <?php
 
+use Renegare\SilexCSH\CookieSessionHandler;
+
 class CookieSessionHandlerTest extends \PHPUnit_Framework_TestCase {
 
-    public function testDummy() {
-        $this->assertTrue(false);
+    public function testSessionHandler() {
+        $cookieHandler = new CookieSessionHandler();
+        $this->assertInstanceOf('SessionHandlerInterface', $cookieHandler);
     }
 }
