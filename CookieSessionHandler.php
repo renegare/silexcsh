da6975e87c83cbd07181eb300d02f23ac2e5844b
Mudi Ugbowanko <mudi.ugbowanko@mendeley.com>
2014-07-02 16:21:14 +0100

    add happy path for read method

--- CookieSessionHandler.php
+++ CookieSessionHandler.php
@@ -2,13 +2,28 @@
 
 namespace Renegare\SilexCSH;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class CookieSessionHandler implements \SessionHandlerInterface {
+
+    /** @var Request */
+    protected $request;
+
+    /** @var string */
+    protected $name;
+
+    /**
+     * @param string $cookieName - name of the cookie that will used to store the session
+     */
+    public function __construct($cookieName) {
+        $this->name = $cookieName;
+    }
 
     /**
      * {@inheritdoc}
      */
     public function close() {
-        throw new \Exception(sprintf('Not Implemented: %s::%s', __CLASS__, __METHOD__));
+        return true;
     }
 
     /**
@@ -28,15 +43,34 @@ class CookieSessionHandler implements \SessionHandlerInterface {
     /**
      * {@inheritdoc}
      */
-    public function open($save_path , $name) {
-        throw new \Exception(sprintf('Not Implemented: %s::%s', __CLASS__, __METHOD__));
+    public function open($savePath, $sessionId) {
+        return $this->getRequest()
+            ->cookies->has($this->name);
     }
 
     /**
      * {@inheritdoc}
      */
     public function read( $session_id) {
-        throw new \Exception(sprintf('Not Implemented: %s::%s', __CLASS__, __METHOD__));
+        $cookies = $this->getRequest()->cookies;
+
+        if(!$cookies->has($this->name)) {
+            return '';
+        }
+
+        $content = @unserialize($cookies->get($this->name));
+
+        if ($content === false || count($content) !== 2) {
+            $content = [time(), ''];
+        }
+
+        list($expire, $data) = $content;
+
+        if($expire !== 0 && $expire < strtotime('now')) {
+            return '';
+        }
+
+        return $data;
     }
 
     /**
@@ -44,5 +78,25 @@ class CookieSessionHandler implements \SessionHandlerInterface {
      */
     public function write($session_id, $session_data) {
         throw new \Exception(sprintf('Not Implemented: %s::%s', __CLASS__, __METHOD__));
+    }
+
+    /**
+     * set request that holds the current cookie session
+     * @param Request $request
+     */
+     public function setRequest(Request $request) {
+         $this->request = $request;
+     }
+
+    /**
+     * get request that holds the current cookie session
+     * @throws RuntimeException
+     * @return Request
+     */
+    protected function getRequest() {
+        if(!$this->request) {
+            throw new \RuntimeException('You cannot access the session without a Request object set');
+        }
+        return $this->request;
     }
 }
--- test/CookieSessionHandlerTest.php
+++ test/CookieSessionHandlerTest.php
@@ -5,7 +5,61 @@ use Renegare\SilexCSH\CookieSessionHandler;
 class CookieSessionHandlerTest extends \PHPUnit_Framework_TestCase {
 
     public function testSessionHandler() {
-        $cookieHandler = new CookieSessionHandler();
-        $this->assertInstanceOf('SessionHandlerInterface', $cookieHandler);
+        $handler = new CookieSessionHandler();
+        $this->assertInstanceOf('SessionHandlerInterface', $handler);
+    }
+
+    public function testCloseAlwaysReturnTrue() {
+        $handler = new CookieSessionHandler();
+        $this->assertTrue($handler->close());
+    }
+
+    public function testDestroyMethod() {
+        $this->markIncomplete();
+    }
+
+    public function testGCMethod() {
+        $this->markIncomplete();
+    }
+
+    public function testOpenMethod() {
+        $handler = new CookieSessionHandler('test_session');
+
+        $request = $this->getMock('Symfony\Component\HttpFoundation\Request');
+        $mockCookieJar = new Symfony\Component\HttpFoundation\ParameterBag;
+        $mockCookieJar->set('test_session', '...');
+        $request->cookies = $mockCookieJar;
+        $handler->setRequest($request);
+
+        $this->assertTrue($handler->open('', ''));
+
+        $mockCookieJar->remove('test_session');
+        $this->assertFalse($handler->open('', ''));
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testOpenMethodException() {
+        $handler = new CookieSessionHandler('test_session');
+        $this->assertTrue($handler->open('', ''));
+    }
+
+    public function testReadMethod() {
+        $handler = new CookieSessionHandler('test_session');
+
+        $sessionData = serialize(['session' => 'data']);
+
+        $payload = serialize([strtotime('1 hour'), $sessionData]);
+
+        $request = $this->getMock('Symfony\Component\HttpFoundation\Request');
+        $mockCookieJar = new Symfony\Component\HttpFoundation\ParameterBag;
+        $mockCookieJar->set('test_session', $payload);
+        $request->cookies = $mockCookieJar;
+        $handler->setRequest($request);
+
+        $this->assertTrue($handler->open('', ''));
+
+        $this->assertEquals($sessionData, $handler->read(''));
     }
 }
