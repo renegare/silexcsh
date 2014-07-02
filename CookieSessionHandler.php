5b87c15b0c3f1a3042ad6d65969d92264b3b0216
Mudi Ugbowanko <mudi.ugbowanko@mendeley.com>
2014-07-02 17:53:58 +0100

    tested cookie session handler

--- CookieSessionHandler.php
+++ CookieSessionHandler.php
@@ -3,6 +3,7 @@
 namespace Renegare\SilexCSH;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class CookieSessionHandler implements \SessionHandlerInterface {
 
@@ -11,12 +12,39 @@ class CookieSessionHandler implements \SessionHandlerInterface {
 
     /** @var string */
     protected $name;
+    protected $path;
+    protected $domain;
+
+    /** @var int */
+    protected $lifeTime;
+
+    /** @var bool */
+    protected $secure;
+    protected $httpOnly;
+
+    /** 
+     * @var mixed(false|null|Cookie) - effects getCookie return value
+     * > false - means no cookie was accessed|written too and therefore no cookie will be returned
+     * > null - means cookie was destroyed
+     * > Cookie - means some data has been written and a cookie will be returned
+     */ 
+    protected $cookie = false;
 
     /**
      * @param string $cookieName - name of the cookie that will used to store the session
+     * @param int             $lifeTime
+     * @param string          $path
+     * @param string          $domain
+     * @param bool            $secure
+     * @param bool            $httpOnly
      */
-    public function __construct($cookieName) {
+    public function __construct($cookieName, $lifeTime = 0, $path = '/', $domain = null, $secure = false, $httpOnly = true) {
         $this->name = $cookieName;
+        $this->lifeTime = $lifeTime;
+        $this->path = $path;
+        $this->domain = $domain;
+        $this->secure = $secure;
+        $this->httpOnly = $httpOnly;
     }
 
     /**
@@ -29,15 +57,16 @@ class CookieSessionHandler implements \SessionHandlerInterface {
     /**
      * {@inheritdoc}
      */
-    public function destroy($session_id) {
-        throw new \Exception(sprintf('Not Implemented: %s::%s', __CLASS__, __METHOD__));
+    public function destroy($sessionId) {
+        $this->cookie = null;
+        return true;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function gc($maxlifetime) {
-        throw new \Exception(sprintf('Not Implemented: %s::%s', __CLASS__, __METHOD__));
+    public function gc($maxLifetime) {
+        return true;
     }
 
     /**
@@ -51,16 +80,13 @@ class CookieSessionHandler implements \SessionHandlerInterface {
     /**
      * {@inheritdoc}
      */
-    public function read( $session_id) {
+    public function read($sessionId) {
         $cookies = $this->getRequest()->cookies;
 
-        if(!$cookies->has($this->name)) {
-            return '';
-        }
+        $content = @unserialize($cookies->get($this->name, ''));
 
-        $content = @unserialize($cookies->get($this->name));
-
-        if ($content === false || count($content) !== 2) {
+        if ($content === false || count($content) !== 2 ||
+            !isset($content[0]) || !isset($content[1])) {
             $content = [time(), ''];
         }
 
@@ -76,17 +102,30 @@ class CookieSessionHandler implements \SessionHandlerInterface {
     /**
      * {@inheritdoc}
      */
-    public function write($session_id, $session_data) {
-        throw new \Exception(sprintf('Not Implemented: %s::%s', __CLASS__, __METHOD__));
+    public function write($sessionId, $sessionData) {
+
+        $expire = $this->lifeTime === 0 ? 0 : strtotime('now') + $this->lifeTime;
+
+        $this->cookie = new Cookie(
+            $this->name,
+            serialize([$expire, $sessionData]),
+            $expire,
+            $this->path,
+            $this->domain,
+            $this->secure,
+            $this->httpOnly
+        );
+
+        return true;
     }
 
     /**
      * set request that holds the current cookie session
      * @param Request $request
      */
-     public function setRequest(Request $request) {
-         $this->request = $request;
-     }
+    public function setRequest(Request $request) {
+        $this->request = $request;
+    }
 
     /**
      * get request that holds the current cookie session
@@ -98,5 +137,21 @@ class CookieSessionHandler implements \SessionHandlerInterface {
             throw new \RuntimeException('You cannot access the session without a Request object set');
         }
         return $this->request;
+    }
+
+    /**
+     * get written cookie or a delete cookie if no cookie has been written
+     * @return mixed false|null|Cookie
+     */
+    public function getCookie() {
+        return $this->cookie;
+    }
+
+    /**
+     * get cookie name
+     * @return string
+     */
+    public function getCookieName() {
+        return $this->name;
     }
 }
--- test/CookieSessionHandlerTest.php
+++ test/CookieSessionHandlerTest.php
@@ -5,21 +5,13 @@ use Renegare\SilexCSH\CookieSessionHandler;
 class CookieSessionHandlerTest extends \PHPUnit_Framework_TestCase {
 
     public function testSessionHandler() {
-        $handler = new CookieSessionHandler();
+        $handler = new CookieSessionHandler('test_session');
         $this->assertInstanceOf('SessionHandlerInterface', $handler);
     }
 
     public function testCloseAlwaysReturnTrue() {
-        $handler = new CookieSessionHandler();
+        $handler = new CookieSessionHandler('test_session');
         $this->assertTrue($handler->close());
-    }
-
-    public function testDestroyMethod() {
-        $this->markIncomplete();
-    }
-
-    public function testGCMethod() {
-        $this->markIncomplete();
     }
 
     public function testOpenMethod() {
@@ -58,8 +50,77 @@ class CookieSessionHandlerTest extends \PHPUnit_Framework_TestCase {
         $request->cookies = $mockCookieJar;
         $handler->setRequest($request);
 
-        $this->assertTrue($handler->open('', ''));
-
         $this->assertEquals($sessionData, $handler->read(''));
+    }
+
+    public function testReadMethodNoSession() {
+        $handler = new CookieSessionHandler('test_session');
+
+        $request = $this->getMock('Symfony\Component\HttpFoundation\Request');
+        $mockCookieJar = new Symfony\Component\HttpFoundation\ParameterBag;
+
+        $request->cookies = $mockCookieJar;
+        $handler->setRequest($request);
+        $this->assertEquals('', $handler->read(''));
+    }
+
+    public function provideInvalidPayloads() {
+        return [
+            [serialize(['session' => 'data'])],
+            [serialize(['session' => 'data', 'hmmm' => 'dssddsd'])],
+            [serialize(['data', 'dssddsd'])],
+            [serialize('12312,jkskjsdkjds')],
+            ['12312,jkskjsdkjds'],
+            [null],
+            [serialize([strtotime('-1 hour'), '{serialized.data}'])]
+        ];
+    }
+
+    /**
+     * @dataProvider provideInvalidPayloads
+     */
+    public function testReadMethodInvalidSessionData($invalidPayload) {
+        $handler = new CookieSessionHandler('test_session');
+
+        $request = $this->getMock('Symfony\Component\HttpFoundation\Request');
+        $mockCookieJar = new Symfony\Component\HttpFoundation\ParameterBag;
+        $mockCookieJar->set('test_session', $invalidPayload);
+        $request->cookies = $mockCookieJar;
+        $handler->setRequest($request);
+
+        $this->assertEquals('', $handler->read(''));
+    }
+
+    public function testWriteMethod() {
+        $handler = new CookieSessionHandler('test_session');
+
+        $sessionData = '{serialized_session_data}';
+        $handler->write('', $sessionData);
+
+        $cookie = $handler->getCookie();
+        $this->assertEquals('test_session', $cookie->getName());
+        $this->assertEquals(serialize([0,$sessionData]), $cookie->getValue());
+        $this->assertNull($cookie->getDomain());
+        $this->assertEquals(0, $cookie->getExpiresTime());
+        $this->assertEquals('/', $cookie->getPath());
+        $this->assertEquals(false, $cookie->isSecure());
+        $this->assertEquals(true, $cookie->isHttpOnly());
+    }
+
+    public function testGCMethod() {
+        $handler = new CookieSessionHandler('test_session');
+        $this->assertTrue($handler->gc(''));
+    }
+
+    public function testDestroyMethod() {
+        $handler = new CookieSessionHandler('test_session');
+        $this->assertFalse($handler->getCookie());
+        $this->assertTrue($handler->destroy(''));
+        $this->assertNull($handler->getCookie());
+    }
+
+    public function testGetCookieName() {
+        $handler = new CookieSessionHandler('test_session');        
+        $this->assertEquals('test_session', $handler->getCookieName());
     }
 }
